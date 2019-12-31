dce9ad55a11a978ddcffccc2d939b45aeb77a48d
Isaiah Awotide <awotideisaiah@yahoo.com>
2019-12-31 16:15:14 +0100

    Updates

---
+++ precache-manifest.9531a871e5d5c44c39166e257db49c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "faec66de237d755d4f26",
+    "url": "awotideisaiah.me/static/js/runtime~main.faec66de.js"
+  },
+  {
+    "revision": "b0cfbc25e44b7722a890",
+    "url": "awotideisaiah.me/static/js/main.b0cfbc25.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "awotideisaiah.me/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b0cfbc25e44b7722a890",
+    "url": "awotideisaiah.me/static/css/main.594a94d0.chunk.css"
+  },
+  {
+    "revision": "b63174cc7f4c0add5405fbad66a24a15",
+    "url": "awotideisaiah.me/index.html"
+  }
+];
